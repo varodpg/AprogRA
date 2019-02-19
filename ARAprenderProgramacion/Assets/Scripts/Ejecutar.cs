8a0d96d324884ce40efab1b0715ed19a0921707a
varodpg <varodpg@gmail.com>
2019-02-19 19:49:56 +0100

    Fluid car movement fixed

--- ARAprenderProgramacion/Assets/Scripts/Ejecutar.cs
+++ ARAprenderProgramacion/Assets/Scripts/Ejecutar.cs
@@ -11,21 +11,21 @@ public class Ejecutar : MonoBehaviour
     public Rigidbody car, star;
     public ArrayList direcciones;
     public Button botonEjecutar;
-    public Vector3 posActual, posDestino, rotacionActual;
+    public Vector3 posActual, posDestino;
+    public Quaternion rotacionDestino, rotacionInicial;
     Vector3 m_EulerAngleVelocity;
     private MovFluido mov;
     public bool mover = false;
+    private bool continuar = false;
     Vector3 pos;
     private int dir = 0;
 
-   
+
 
 
     void Start()
     {
         botonEjecutar.onClick.AddListener(NewMethod);
-        posActual = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z);
-
     }
 
     private void NewMethod()
@@ -44,11 +44,6 @@ public class Ejecutar : MonoBehaviour
         return posDestino;
     }
 
-    public Vector3 getRotActual()
-    {
-        return rotacionActual;
-    }
-
     public void setPosActual(Vector3 pos)
     {
         posActual = pos;
@@ -58,126 +53,158 @@ public class Ejecutar : MonoBehaviour
         posDestino = pos;
     }
 
-    public void setRotActual(Vector3 rot)
-    {
-        rotacionActual = rot;
-    }
-
     public Vector3 getEuler()
     {
         return m_EulerAngleVelocity;
     }
 
-#endregion
+    #endregion
 
-    //public void aniadirDireccion(int dir)
-    //{
-    //    if (direcciones == null)
-    //    {
-    //        direcciones = new ArrayList(5);
-    //        direcciones.Add(dir); // se añade por el final
-    //        Debug.Log("POSICION INICIAL: " + car.position.x);
-    //        Debug.Log("Añadida primera direccion");
-    //    }
-    //    else
-    //    {
-    //        direcciones.Add(dir); // se añade por el final
-    //        Debug.Log("un marcador ha añadido una direccion");
-    //        for (int i = 0; i < direcciones.Count; i++)
-    //        {
-    //            Debug.Log(direcciones[i]);
-    //        }
 
-    //    }
-
-    //}
     public void mostrarArray(ArrayList direcciones)
     {
         Debug.Log("Inicio del array");
-        for (int  i = 0;  i < direcciones.Count;  i++)
+        for (int i = 0; i < direcciones.Count; i++)
         {
             Debug.Log(direcciones[i]);
         }
         Debug.Log("Fin del array");
     }
-    public void ejecutarMovimiento(ArrayList dirs) //añadir al OnClick() del boton ejecutar
+    public void ejecutarMovimiento(ArrayList dirs) // (1.3f * 0.3f)
     {
-        posActual = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z);
-        mostrarArray(direcciones);
-        // if dirs está vacío mostrar canvas (usa los marcadores)
-        try
-        {
-            for (int i = 0; i < dirs.Count; i++)
-            {
-                
-                switch (dirs[i])
-                {
-                    case 1: //arriba
-                        posDestino = new Vector3(car.transform.position.x, car.transform.position.y, posActual.z + (1.3f * 0.3f));
-                        m_EulerAngleVelocity = new Vector3(0, 100, 0);
-                        dir = 1;
-                        break;
-                    case 2: //derecha
-                        dir = 2;
-                        posDestino = new Vector3(posActual.x + (1.3f * 0.3f), car.transform.position.y, car.transform.position.z);
-                        m_EulerAngleVelocity = new Vector3(0, 100, 0);
-                        Debug.Log("Angulo: "+car.transform.rotation.eulerAngles.y);
-                        if (car.transform.rotation.eulerAngles.y > 90.1)
-                        {
-                            car.transform.Rotate(Vector3.up, -90);
-                        }
-                        if (car.transform.rotation.eulerAngles.y < 90)
-                        {
-                            car.transform.Rotate(Vector3.up, 90);
-                        }
+        StartCoroutine(Wait(dirs));
+    }
+    
 
-                        mover = true;
-                        //posActual = posDestino;
-                        
-                        break;
-                    case 3: //abajo
-                        dir = 3;
-                        posDestino = new Vector3(posActual.x , car.transform.position.y, car.transform.position.z - (1.3f * 0.3f));
-                        
-                      
-                        break;
-                    case 4: //izquierda
-                        dir = 4;
-                        posDestino = new Vector3(posActual.x , car.transform.position.y, car.transform.position.z +(1.3f * 0.3f));
-        
-                        
-                        break;
-                    default:
-                        break; // podemos mostrar un mensaje por pantalla diciendo que no hay ningún movimiento.
+    private void Update()
+    {
+        if (mover == true)
+        {
+            if (dir == 2) //derecha
+            {
+                if  (posDestino.x - car.position.x >= 0.01f)
+                {
+                    if ((90.1f < car.transform.rotation.eulerAngles.y) & (89.9f > car.transform.rotation.eulerAngles.y))
+                    {
+                        car.transform.rotation = Quaternion.Slerp(car.transform.rotation, rotacionDestino, Time.deltaTime * 5.0f);
+                    }
+                    car.MovePosition(new Vector3(car.position.x + 0.01f, car.position.y, car.position.z));
                 }
-                //car.MovePosition(Vector3.MoveTowards(posActual, posDestino, 1));
-               
                 
 
             }
-        }
-        catch (System.Exception)
-        {
-            Debug.Log("No hay elementos en el array");
-        }
+            else if (dir == 3)//abajo
+            {
+                Debug.Log("Primer paso");
+                if (car.position.z - posDestino.z >= 0.01)
+                {
+                    if ((180.1f < car.transform.rotation.eulerAngles.y) & (179.9f > car.transform.rotation.eulerAngles.y))
+                    {
+                        car.transform.rotation = Quaternion.Slerp(car.transform.rotation, rotacionDestino, Time.deltaTime * 5.0f);
+                    }
+                    Debug.Log("Cuarto paso");
+                    car.MovePosition(new Vector3(car.position.x, car.position.y, car.position.z - 0.01f));
+                }
+            }
+            else if (dir == 1) //arriba
+            {
+                if (car.position.z - posDestino.z <= 0.01)
+                {
+                    if ((0.1f < car.transform.rotation.eulerAngles.y) & (0.9f > car.transform.rotation.eulerAngles.y))
+                    {
+                        car.transform.rotation = Quaternion.Slerp(car.transform.rotation, rotacionDestino, Time.deltaTime * 5.0f);
+                    }
+                    car.MovePosition(new Vector3(car.position.x, car.position.y, car.position.z + 0.01f));
+                }
+            }
+            else if (dir == 4)//izquierda
+            {
+                Debug.Log("Posicion resultante: " + (posDestino.x - car.position.x));
+                if ((posDestino.x < car.position.x)) 
+                {
+                    //if (car.transform.rotation.y != rotacionDestino.y)
+                    {
+                        Debug.Log("Rotacion actual: " + car.rotation.y);
+                        if ((270.1f < car.transform.rotation.eulerAngles.y) & (269.9f > car.transform.rotation.eulerAngles.y))
+                        {
+                            car.transform.rotation = Quaternion.Slerp(car.transform.rotation, rotacionDestino, Time.deltaTime * 5.0f);
+                        }
+                        Debug.Log("Segunda Rotacion actual: " + car.rotation.y);
+                        Debug.Log("Supuesta Rotacion destino: " + rotacionDestino);
+
+                    }
+                    car.MovePosition(new Vector3(car.position.x - 0.01f, car.position.y, car.position.z));
+                }
 
 
-    }
-
- 
-
-    private void FixedUpdate()
-    {
-       
-        if (mover == true) {
-            car.transform.position = car.transform.position + (new Vector3(0.002f,0,0));
-            posActual = car.transform.position;
-            if (posActual == posDestino)
+            }
+            else
             {
                 mover = false;
             }
             
         }
     }
+
+    IEnumerator Wait(ArrayList direc)
+    {
+        posActual = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z);
+        for (int i = 0; i < direc.Count; i++)
+        {
+            
+            if (direc[i].Equals(1)) //arriba
+            {
+                posDestino = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z + (1.3f * 0.3f));
+                rotacionDestino = Quaternion.Euler(car.rotation.x, 0, car.rotation.z);
+                yield return new WaitForSeconds(1.5f);
+                Debug.Log("Ejecutando la posicion: " + i + ":" + direc[i]);
+                dir = 1;
+                mover = true;
+                yield return new WaitForSeconds(1.5f);
+            }
+            if (direc[i].Equals(2)) //derecha
+            {
+
+                posDestino = new Vector3(car.transform.position.x + (1.3f * 0.3f), car.transform.position.y, car.transform.position.z);
+                rotacionDestino = Quaternion.Euler(car.rotation.x, 90, car.rotation.z);
+                yield return new WaitForSeconds(1.5f);
+                Debug.Log("Ejecutando la posicion: " + i + ":" + direc[i]);
+                dir = 2;
+                mover = true;
+                
+                
+                yield return new WaitForSeconds(1.5f); 
+                
+
+            }
+            if (direc[i].Equals(3)) //abajo
+            {
+                posDestino = new Vector3(car.transform.position.x , car.transform.position.y, car.transform.position.z - (1.3f * 0.3f));
+                rotacionDestino = Quaternion.Euler(car.rotation.x, 180, car.rotation.z);
+                yield return new WaitForSeconds(1.5f);
+                Debug.Log("Ejecutando la posicion: " + i + ":" + direc[i]);
+                dir = 3;
+                mover = true;
+                yield return new WaitForSeconds(1.5f);
+            }
+            if (direc[i].Equals(4)) //izquierda
+            {
+                posDestino = new Vector3(car.transform.position.x - (1.3f * 0.3f), car.transform.position.y, car.transform.position.z);
+                rotacionDestino = Quaternion.Euler(car.rotation.x, 270, car.rotation.z);
+                yield return new WaitForSeconds(1.5f);
+                Debug.Log("Ejecutando la posicion: " + i + ":" + direc[i]);
+                dir = 4;
+                mover = true;
+
+
+                yield return new WaitForSeconds(1.5f);
+            }
+            posActual = posDestino;
+        }
+
+    }
+
+
+
 
 }
